bd84eb2b1339a0efe0139e064c869961beab6d70
GormYa <gormya@gmail.com>
2017-06-21 12:08:26 +0300

    First commit

--- CardReader/main.cpp
+++ CardReader/main.cpp
@@ -2,28 +2,22 @@
 #include "bcm2835.h"
 #include <iostream>
 
-MFRC522::MIFARE_Key key;
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Turkish");
 	cout << "Kartı okutunuz..." << endl;
+	
 	MFRC522 mfrc;
-
 	mfrc.PCD_Init();
 
-	for (byte i = 0; i < 6; i++) {
-		key.keyByte[i] = 0xFF;
-	}
-
-	while (1) {
+	while (true) {
 		if (!mfrc.PICC_IsNewCardPresent())
 			continue;
 
 		if (!mfrc.PICC_ReadCardSerial())
 			continue;
 
-		// Print UID
-		printf("UID: ");
+		printf("Kart seri no: ");
 		for (byte i = 0; i < mfrc.uid.size; ++i) {
 			if (mfrc.uid.uidByte[i] < 0x10) {
 				printf(" 0");
